2e03cc7dc8457c469323d7ae58ff4e01664183a2
ivan <ivan.jaraiz@gmail.com>
2020-02-25 19:03:48 +0100

    codigo mux4a1

---
+++ BloquesLogicos/multiplexor_4 entrada_1_salida.vhd
@@ -0,0 +1,22 @@
+
+entity mux4a1 is
+	port( enable : in std_logic;
+	      x : in std_logic_vector (3 downto 0);
+		  sel : in std_logic_vector (1 downto 0);
+		  y : out std_logic);
+	end mux4a1;
+architecture functional of mux4a1 is
+begin
+	process(enable, x, sel)
+	begin
+	 if enable = '0' then <= '0';
+	   else
+		  case sel is
+			when "00" => y <= x(0);
+			when "01" => y <= x(1);
+			when "10" => y <= x(2);
+			when "11" => y <= x(3);
+		  end case;
+	  end if;
+	end process;
+end functional;
